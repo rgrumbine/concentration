8b0aff56a8f72c82c2f0988373b2d27427a45ece
Robert Grumbine <Robert.Grumbine@noaa.gov>
2021-04-13 21:32:14 +0000

    start towards running

--- ecf/iceconc.runup.sh
+++ ecf/iceconc.runup.sh
@@ -12,42 +12,60 @@
 #####
 
 
+set -e
+#debug env > env.10 2> env.20
+#acorn
+    source /apps/prod/lmodules/startLmod
 #-----------------------------------------------------------------------------
 echo zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz loading modules zzzzzzzzzzzzzzzzzzzzzzz
 module purge
-module load EnvVars/1.0.3
-module load ips/19.0.5.281 impi/19.0.5
-module load prod_envir/1.1.0
-module load prod_util/1.1.5
-module load grib_util/1.1.1
-module load bufr_dumplist/2.3.0
-module load dumpjb/5.1.0
-module load imagemagick/6.9.9-25
-module load lsf/10.1 #for internal job management, i.e., bkill
+#phase3
+#module load EnvVars/1.0.3
+#module load ips/19.0.5.281 impi/19.0.5
+#module load prod_envir/1.1.0
+#module load prod_util/1.1.5
+#module load grib_util/1.1.1
+#module load bufr_dumplist/2.3.0
+#module load dumpjb/5.1.0
+#module load imagemagick/6.9.9-25
+#module load lsf/10.1 #for internal job management, i.e., bkill
+#acorn
+    module load envvar/1.0
+    module load PrgEnv-intel
+    module load intel/19.1.3.304
+    module load intel/19.1.3.304/cray-mpich/8.1.2
+    #module load bacio bufr g2
+    #module load libpng/1.6.37
+    #module load zlib/1.2.11
+    #module load jasper/2.0.16
+    module load netcdf/4.7.4
+
 module list
 echo zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz done loading modules zzzzzzzzzzzzzzzzzz
 
 # Bring the various environment-sensitive definitions out of J jobs and to here:
 #NCO refers to these as 'job card' variables
-set -x
+set -xe
 
-tagm=20210408
-tag=20210409
-end=20210409
+tagm=20210324
+tag=20210325
+end=20210325
 
 export HOMEbase=/u/Robert.Grumbine/rgdev
 export seaice_analysis_ver=v4.4.0
 export HOMEseaice_analysis=$HOMEbase/seaice_analysis.${seaice_analysis_ver}
 
 #Use this to override system in favor of my archive:
-export DCOMROOT=/u/Robert.Grumbine/noscrub/satellites/
+export DCOMROOT=/u/Robert.Grumbine/save/
 export RGTAG=prod
 export my_archive=true
 
 cd $HOMEseaice_analysis/ecf
 
-export COMINsst_base=/u/Robert.Grumbine/noscrub/sst/prod/sst
+export COMINsst_base=/u/Robert.Grumbine/save/sst/prod/sst
+#debug env > env.1 2> env.2
 . ./jobcards
+#debug env > env.1b 2> env.2b
 
 if [ -z $obsproc_dump_ver ] ; then
   echo null obsproc_dump_ver
